f911b2734d1fc11b6aa1760a6ec478ce16291924
chaeyoungieee02 <timquirstenavery.tan@tup.edu.ph>
2026-07-07 21:27:10 +0800

    Delete advwebapp-main directory

--- advwebapp-main/backend/models/orderItem.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('OrderItem', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    order_id: { type: DataTypes.INTEGER, allowNull: false },
-    product_id: { type: DataTypes.INTEGER, allowNull: false },
-    quantity: { type: DataTypes.INTEGER, allowNull: false },
-    size: DataTypes.STRING
-  }, { tableName: 'order_items', timestamps: true });
-};
--- advwebapp-main/backend/routes/dashboard.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getDashboardStats } = require('../controllers/dashboard');
-const { isAdmin } = require('../middlewares/auth');
-
-router.get('/dashboard', isAdmin, getDashboardStats);
-
-module.exports = router;
